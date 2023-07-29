94ca9db681cdbc1315f3823c934ebfdc554c24a8
Romain Berthollet <romanberth@gmail.com>
2023-07-29 12:24:23 +0200

    :zap: add methods to access redis functionality

--- app/src/Common/Redis/RedisInterface.php
+++ app/src/Common/Redis/RedisInterface.php
@@ -2,8 +2,15 @@
 
 namespace App\Common\Redis;
 
+use Predis\ClientInterface;
+use Symfony\Component\Cache\Adapter\RedisAdapter;
+
 interface RedisInterface
 {
+    public function getRedisClient(): ClientInterface;
+
+    public function getCacheAdapter(): RedisAdapter;
+
     public function getItem(string $key): mixed;
 
     public function saveItem(string $key, mixed $value): mixed;
